857b3e19d45e495da2a22ed36fd377605c4f2f19
Vincent Landgraf <vincent@landgrafx.de>
2021-06-27 15:22:33 +0200

    add block device mode

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-finbin:	cmd/finbin/*.go
-	GO_ENABLED=0 go build -a -ldflags '-extldflags "-static"' -o $@ ./cmd/finbin
+finbin.amd64.linux:	cmd/finbin/*.go
+	GO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags '-extldflags "-static"' -o $@ ./cmd/finbin
--- cmd/finbin/main.go
+++ cmd/finbin/main.go
@@ -9,12 +9,13 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strconv"
 
 	"github.com/inhies/go-bytesize"
 )
 
 var (
-	file, pattern, size, dir string
+	file, pattern, size, dir, skip string
 )
 
 func main() {
@@ -22,11 +23,17 @@ func main() {
 	flag.StringVar(&pattern, "pattern", "testdata", "regex pattern to find")
 	flag.StringVar(&size, "size", "10B", "size to capture before and after")
 	flag.StringVar(&dir, "dir", "found", "path to store found records")
+	flag.StringVar(&skip, "skip", "0", "skip over bytes from the beginning of the file")
 	flag.Parse()
 
 	bs, err := bytesize.Parse(size)
 	if err != nil {
 		log.Fatalf("invalid size %q: %v", size, err)
+	}
+
+	sb, err := strconv.ParseInt(skip, 10, 64)
+	if err != nil {
+		log.Fatalf("invalid skip %q: %v", skip, err)
 	}
 
 	err = os.MkdirAll(dir, os.FileMode(0755))
@@ -38,6 +45,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("unable to stat file %q: %v", file, err)
 	}
+	if s.Size() == 0 {
+		log.Println("Block device mode.")
+	}
 
 	fh, err := os.Open(file)
 	if err != nil {
@@ -46,9 +56,15 @@ func main() {
 	defer fh.Close()
 
 	regex, err := regexp.Compile(pattern)
-
 	if err != nil {
 		log.Fatalf("unable to compile pattern %q: %v", pattern, err)
+	}
+
+	if sb > 0 {
+		_, err := fh.Seek(sb, io.SeekStart)
+		if err != nil {
+			log.Fatalf("unable to skip in %q: %v", pattern, err)
+		}
 	}
 
 	for {
@@ -69,15 +85,19 @@ func main() {
 			start = 0
 		}
 		end := offset + int64(positions[1]) + int64(bs)
-		if end > s.Size() {
+		if end > s.Size() && s.Size() != 0 {
 			end = s.Size()
 		}
 		total := end - start
 
-		outFile := fmt.Sprintf("file-%d", positions[0])
+		outFile := fmt.Sprintf("file-%d", offset+int64(positions[0]))
 
-		fmt.Printf("Found %s (%d to %d size %v)\n",
-			outFile, start, end, bytesize.ByteSize(total))
+		fmt.Printf("Found %s - copy section from %d (%v) to %d (%v) size %v\n",
+			outFile,
+			start, bytesize.ByteSize(start),
+			end, bytesize.ByteSize(end),
+			bytesize.ByteSize(total),
+		)
 
 		outFilePath := filepath.Join(dir, outFile)
 		ofh, err := os.Create(outFilePath)
@@ -91,9 +111,19 @@ func main() {
 			log.Fatalf("unable to seek to %d in file %q: %v", start, file, err)
 		}
 
-		_, err = io.CopyN(ofh, fh, total)
-		if err != nil {
-			log.Fatalf("unable to copy data to file %q: %v", outFilePath, err)
+		n, err := io.CopyN(ofh, fh, total)
+		if s.Size() == 0 { // blockdevice mode
+			if err != nil {
+				if n > 0 && n < total {
+					// ignore
+				} else {
+					log.Fatalf("unable to copy data to file %q: %v", outFilePath, err)
+				}
+			}
+		} else {
+			if err != nil {
+				log.Fatalf("unable to copy data to file %q: %v", outFilePath, err)
+			}
 		}
 
 		ofh.Close()
